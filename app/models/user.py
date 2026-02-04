4b2c3a97fcbff0fd69652c245a5595cfb1743e74
Abdul Rehman <abdulrehman007033@gmail.com>
2026-02-04 16:24:23 +0500

    Remove __init__file

---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    projects = relationship('Project', back_populates='owner')
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from app.models import User
+from app.models.user import User
 
 class UserRepository:
     def __init__(self, db: Session):
